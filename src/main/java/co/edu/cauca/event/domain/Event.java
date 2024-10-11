565d1680463ec903b2a4e589c3216f8a75efe1a7
Ashlee Campaz <ashleecampaz@unicauca.edu.co>
2024-10-11 00:24:12 -0500

    agregue las clases chair y researcher

---
+++ src/main/java/co/edu/cauca/event/domain/Event.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.cauca.event.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Ashlee Vanessa Campaz
+ */
+@Entity
+@Table (name = "event")
+public class Event implements Serializable {
+    String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
